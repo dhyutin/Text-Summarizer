ac45332eeb382ff95124e177462e04200257ddfa
dhyutin <62614350+dhyutin@users.noreply.github.com>
2023-12-15 18:16:54 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from textSummarizer.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage2_data_validation import DataValidationTrainingPipeline
 from textSummarizer.loggging import logger
 
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion Stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,11 +1,20 @@
-# ENTITY - dATA iNGESTION
+
 from dataclasses import dataclass
 from pathlib import Path
 
 #Define a decorator called 'dataclass'
+# ENTITY - dATA iNGESTION
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+# ENTITY - dATA Validation
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
